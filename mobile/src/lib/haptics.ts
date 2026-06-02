532609cb50ae62f802b6654003e653d42b17a46d
ZY-GN <zy@gradient.network>
2026-06-02 18:25:05 +0800

    feat(mobile): selection + heavy haptics

--- mobile/src/lib/haptics.ts
+++ mobile/src/lib/haptics.ts
@@ -2,3 +2,5 @@ import * as Haptics from 'expo-haptics'
 export const tapLight = () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
 export const tapMedium = () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
 export const success = () => Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+export const selection = () => Haptics.selectionAsync()
+export const tapHeavy = () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
